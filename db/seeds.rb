07dea6fe243ba19d6ca3919a66b6be58f1d5a49b
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-19 23:12:33 +0200

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,84 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+customers = Customer.create([
+  { 
+    name: "John Doe",
+    email: "pierreadelkamel@gmail.com",
+    password: "password1"
+  }, 
+
+  { 
+    name: "Doe John",
+    email: "Doe@gmail.com",
+    password: "password1"
+  }, 
+
+  { 
+    name: "John John",
+    email: "John@gmail.com",
+    password: "password1"
+  }, 
+])
+
+admins = Admin.create([
+  { 
+    name: "Admin John Doe",
+    email: "Adminpierreadelkamel@gmail.com",
+    password_digest: "password1"
+  }, 
+
+  { 
+    name: "Admin Doe John",
+    email: "AdminDoe@gmail.com",
+    password_digest: "password1"
+  }, 
+
+  { 
+    name: "Admin John John",
+    email: "AdminJohn@gmail.com",
+    password_digest: "password1"
+  }, 
+])
+
+reservations = Reservation.create([
+  { 
+    date_from: DateTime.strptime("09/14/2022", "%m/%d/%Y"),
+    date_to: DateTime.strptime("09/16/2022", "%m/%d/%Y"),
+    hotel: Hotel.first,
+    customer: Customer.first
+  }, 
+  { 
+    date_from: DateTime.strptime("09/14/2022", "%m/%d/%Y"),
+    date_to: DateTime.strptime("09/17/2022", "%m/%d/%Y"),
+    hotel: Hotel.last,
+    customer: Customer.last
+  }, 
+  
+])
+
+
+hotels = Hotel.create([
+  { 
+    cover_image_url: "https://media.istockphoto.com/photos/luxury-resort-picture-id104731717?k=20&m=104731717&s=612x612&w=0&h=40INtJRzhmU1O4Rj24zdY8vj4aGsWpPaEfojaVQ8xBo=",
+    city: "Cairo",
+    hotel_name: "New Cairo Hotel",
+    score: 5,
+    price_per_night:20.25,
+    admin: Admin.first
+  }, 
+  { 
+    cover_image_url: "https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=1024x768",
+    city: "Giza",
+    hotel_name: "New Giza Hotel",
+    score: 2,
+    price_per_night:99,
+    admin: Admin.last
+  },  
+  
+])
+
+
+
