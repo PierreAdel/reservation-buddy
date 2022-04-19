0b19083402b9e5ead129eb89819fb2fe6aded522
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-19 23:13:56 +0200

    Update routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :hotels, param: :slug
+      resources :reservations, only: [:index, :show]
+      resources :admins, only: [:index, :show]
+      resources :customers, only: [:index, :show]
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
 end
