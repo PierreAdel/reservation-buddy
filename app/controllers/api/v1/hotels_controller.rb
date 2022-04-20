798af741a440b0c14786e90c4ce53a910ca76ed4
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-20 18:05:50 +0200

    Create hotels controller

---
+++ app/controllers/api/v1/hotels_controller.rb
@@ -0,0 +1,54 @@
+module Api
+    module V1
+    class HotelsController < ApplicationController
+        protect_from_forgery with: :null_session
+        def index
+            hotels = Hotel.all
+            render json: HotelSerializer.new(hotels).serialized_json
+        end
+
+        def show
+            hotel = Hotel.find_by(slug: params[:slug])
+            # render json: HotelSerializer.new(hotel, options).serialized_json
+            render json: HotelSerializer.new(hotel).serialized_json
+        end
+
+        def create
+            hotel = Hotel.new(hotel_params)
+            if hotel.save
+                render json: HotelSerializer.new(hotel).serialized_json
+            else
+                render json: { errors: hotel.errors.full_messages }, status: 422
+            end
+        end
+
+        def update
+            hotel = Hotel.find_by(slug: params[:slug])
+            if hotel.update(hotel_params)
+                # render json: HotelSerializer.new(hotel, options).serialized_json
+                render json: HotelSerializer.new(hotel).serialized_json
+            else
+                render json: { errors: hotel.errors.full_messages }, status: 422
+            end
+        end
+
+        def destroy
+            hotel = Hotel.find_by(slug: params[:slug])
+            if hotel.destroy
+                head :no_content
+            else
+                render json: { errors: hotel.errors.full_messages }, status: 422
+            end
+        end
+
+        private
+        def hotel_params
+            params.require(:hotel).permit(:name, :cover_image_url)
+        end
+
+        # def options 
+        #     @options ||= {include: %i[admins]}
+        # end
+    end
+    end
+end
