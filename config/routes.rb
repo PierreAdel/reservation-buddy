f1470fd22d847e51ff7af43ddca519b10ffa2e04
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-20 19:59:55 +0200

    Update routes config

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
+      delete '/sessions/logout', to: "sessions#logout"
+      get "sessions/logged_in", to: "sessions#logged_in"
       resources :hotels, param: :slug
       resources :reservations, only: [:index, :show]
       resources :admins, only: [:index, :show]
