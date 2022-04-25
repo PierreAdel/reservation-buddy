c1768afedea4233fc51fbbf71490462081b2366d
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-26 00:58:58 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_213105) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_25_200112) do
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -43,11 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_213105) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "date_from"
-    t.date "date_to"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id", null: false
     t.integer "hotel_id", null: false
+    t.integer "number_of_days"
     t.index ["customer_id"], name: "index_reservations_on_customer_id"
     t.index ["hotel_id"], name: "index_reservations_on_hotel_id"
   end
