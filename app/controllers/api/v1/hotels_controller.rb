558f742ac4e6dce73b3a9ada1ff8d075bbb03d95
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-26 00:45:00 +0200

    Update hotels controller

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -1,24 +1,32 @@
 module Api
     module V1
     class HotelsController < ApplicationController
-        protect_from_forgery with: :null_session
-        before_action :authenticate_user, only: [:show]
-
+        # protect_from_forgery with: :null_session
+        before_action :authenticate_admin, only: [:create, :update, :destroy]
+        MAX_PAGINATION_LIMIT = 100
         def index
-            hotels = Hotel.all
-            render json: HotelSerializer.new(hotels).serialized_json
+            # binding.irb
+            hotels = Hotel.all.limit(limit).offset(params[:offset]).order(score: :asc).where("city LIKE '%#{params.fetch(:search,"")}%'")
+            render json: HotelsRepresenter.new(hotels).as_json
         end
 
         def show
             hotel = Hotel.find_by(slug: params[:slug])
-            # render json: HotelSerializer.new(hotel, options).serialized_json
-            render json: HotelSerializer.new(hotel).serialized_json
+            render json: HotelRepresenter.new(hotel).as_json
+
+            # admin = Admin.find(hotel.admin_id)
+            # render json: admin
+            
+            # hotel =  Hotel
+            # .select('hotels.*, admins.name as admin_name')
+            # .joins("INNER JOIN admins ON admins.id = hotels.admin_id")
+            # render json: hotel 
         end
 
         def create
-            hotel = Hotel.new(hotel_params)
+            hotel = Hotel.new(hotel_params.merge( admin_id: helpers.current_admin.id))
             if hotel.save
-                render json: HotelSerializer.new(hotel).serialized_json
+                render json: HotelRepresenter.new(hotel).as_json , status: :created
             else
                 render json: { errors: hotel.errors.full_messages }, status: 422
             end
@@ -26,9 +34,11 @@ module Api
 
         def update
             hotel = Hotel.find_by(slug: params[:slug])
-            if hotel.update(hotel_params)
+            if !hotel
+                render json: { errors: "Hotel not found" }, status: 404
+            elsif hotel.update(hotel_params)
                 # render json: HotelSerializer.new(hotel, options).serialized_json
-                render json: HotelSerializer.new(hotel).serialized_json
+                render json: hotel
             else
                 render json: { errors: hotel.errors.full_messages }, status: 422
             end
@@ -36,7 +46,9 @@ module Api
 
         def destroy
             hotel = Hotel.find_by(slug: params[:slug])
-            if hotel.destroy
+            if !hotel
+                render json: { errors: "Hotel not found" }, status: 404
+            elsif hotel.destroy
                 head :no_content
             else
                 render json: { errors: hotel.errors.full_messages }, status: 422
@@ -44,12 +56,16 @@ module Api
         end
 
         private
-        def hotel_params
-            params.require(:hotel).permit(:name, :cover_image_url)
+        def limit
+            [params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i, MAX_PAGINATION_LIMIT].min
         end
 
-        def authenticate_user
-            unless helpers.logged_in?
+        def hotel_params
+            params.require(:hotel).permit(:hotel_name, :cover_image_url, :description, :city, :score, :price_per_night)
+        end
+
+        def authenticate_admin
+            unless helpers.admin_logged_in?
               render json: { errors: "Not Authorized" }, status: :unauthorized
             end
           end
