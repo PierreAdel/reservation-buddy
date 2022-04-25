0bb1cbfa5a52bf49d28982a5e2f5a92c27133e44
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-26 00:45:37 +0200

    Update sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,9 @@ module Api
     module V1
     class SessionsController < ApplicationController
       include CurrentUserConcern
+      include CurrentAdminConcern
     
-        def create
+        def user_login
             @user = Customer.find_by(email: params[:email])
             if @user && @user.authenticate(params[:password])
                 session[:user_id] = @user.id
@@ -17,7 +18,23 @@ module Api
             end
         end
 
-        def logged_in
+        def admin_login
+            @admin = Admin.find_by(email: params[:email])
+            if @admin && @admin.authenticate(params[:password])
+                session[:admin_id] = @admin.id
+                render json: {
+                    status: :created,
+                    logged_in: true,
+                    admin: @admin
+                }
+            else
+                render json: { status: 401 }
+            end
+        end
+
+
+
+        def user_logged_in
             if @current_user
                 render json: {
                     logged_in: true,
@@ -30,7 +47,25 @@ module Api
             end
         end
 
-        def logout
+        def admin_logged_in
+            if @current_admin
+                render json: {
+                    logged_in: true,
+                    admin: @current_admin
+                }
+            else
+                render json: {
+                    logged_in: false
+                }
+            end
+        end
+
+        def user_logout
+            reset_session
+            render json: { status: 200, logged_out: true }
+        end
+
+        def admin_logout
             reset_session
             render json: { status: 200, logged_out: true }
         end
