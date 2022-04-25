f417d011de3e7fcdf021fb652216ec5c7f62294c
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-26 00:56:29 +0200

    Update hotel model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -3,6 +3,13 @@ class Hotel < ApplicationRecord
     has_many :reservations
 
     before_create :slugify
+    before_update :slugify
+
+    validates :hotel_name, uniqueness: true, presence: true, length: { minimum: 2 }
+    validates :city, presence: true, length: { minimum: 2 }
+    validates :cover_image_url, presence: true, length: { minimum: 2 }
+    validates :score, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+    validates :price_per_night, numericality: { greater_than: 0, less_than_or_equal_to: 10000 }
 
     def slugify
     self.slug = hotel_name.parameterize
