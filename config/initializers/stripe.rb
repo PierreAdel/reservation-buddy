0d592cd5468fd379ad0a7fea23e309ff3c3ec726
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-26 00:57:46 +0200

    Create stripe initalizer

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,5 @@
+# Rails.configuration.stripe = {
+#     :publishable_key => ENV['PUBLISHABLE_KEY'],
+#     :secret_key => ENV['STRIPE_SECRET_KEY']
+# }
+# stripe.api_key = Rails.configuration.stripe[:secret_key]
