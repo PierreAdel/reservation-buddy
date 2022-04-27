3a2c888c73e708ef78caeaf9f40d31efcc087cc0
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-27 22:29:30 +0200

    Update sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,74 +1,70 @@
 module Api
-    module V1
+  module V1
     class SessionsController < ApplicationController
       include CurrentUserConcern
       include CurrentAdminConcern
-    
-        def user_login
-            @user = Customer.find_by(email: params[:email])
-            if @user && @user.authenticate(params[:password])
-                session[:user_id] = @user.id
-                render json: {
-                    status: :created,
-                    logged_in: true,
-                    user: @user
-                }
-            else
-                render json: { status: 401 }
-            end
+
+      def user_login
+        @user = Customer.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          render json: {
+                   logged_in: true,
+                   user: CustomerRepresenter.new(@user).as_json,
+                 },
+                 status: :created
+        else
+          render json: {}, status: 401
         end
+      end
 
-        def admin_login
-            @admin = Admin.find_by(email: params[:email])
-            if @admin && @admin.authenticate(params[:password])
-                session[:admin_id] = @admin.id
-                render json: {
-                    status: :created,
-                    logged_in: true,
-                    admin: @admin
-                }
-            else
-                render json: { status: 401 }
-            end
+      def admin_login
+        @admin = Admin.find_by(email: params[:email])
+        if @admin && @admin.authenticate(params[:password])
+          session[:admin_id] = @admin.id
+          render json: {
+                   logged_in: true,
+                   admin: AdminRepresenter.new(@admin).as_json,
+                 },
+                 status: :created
+        else
+          render json: {}, status: 401
         end
+      end
 
-
-
-        def user_logged_in
-            if @current_user
-                render json: {
-                    logged_in: true,
-                    user: @current_user
-                }
-            else
-                render json: {
-                    logged_in: false
-                }
-            end
+      def user_logged_in
+        if @current_user
+          render json: {
+                   logged_in: true,
+                   user: CustomerRepresenter.new(@current_user).as_json,
+                 },
+                 status: :ok
+        else
+          render json: { logged_in: false }, status: :ok
         end
+      end
 
-        def admin_logged_in
-            if @current_admin
-                render json: {
-                    logged_in: true,
-                    admin: @current_admin
-                }
-            else
-                render json: {
-                    logged_in: false
-                }
-            end
+      def admin_logged_in
+        if @current_admin
+          render json: {
+                   logged_in: true,
+                   admin: AdminRepresenter.new(@current_admin).as_json,
+                 },
+                 status: :ok
+        else
+          render json: { logged_in: false }, status: :ok
         end
+      end
 
-        def user_logout
-            reset_session
-            render json: { status: 200, logged_out: true }
-        end
+      def user_logout
+        reset_session
+        render json: { logged_out: true }, status: :ok
+      end
 
-        def admin_logout
-            reset_session
-            render json: { status: 200, logged_out: true }
-        end
+      def admin_logout
+        reset_session
+        render json: { logged_out: true }, status: :ok
+      end
     end
-end
+  end
 end
