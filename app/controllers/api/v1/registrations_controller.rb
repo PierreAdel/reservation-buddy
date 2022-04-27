68f3882041f6085d2cd0406b3f3c0f80c6dd19fc
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-27 22:29:22 +0200

    Update registrations controller

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,22 +1,25 @@
 module Api
-    module V1
+  module V1
     class RegistrationsController < ApplicationController
-        
-       
-        def create
-            @user = Customer.new(name: params[:name], email: params[ :email], password: params[:password], password_confirmation: params[:password])
-            p @user
-            if @user.save
-                session[:user_id] = @user.id 
-                render json: {
-                    status: :created,
-                    logged_in: true,
-                    user: @user
-                }
-            else
-                render json: { status: 500 }
-            end
+      def create
+        @user =
+          Customer.new(
+            name: params[:name],
+            email: params[:email],
+            password: params[:password],
+            password_confirmation: params[:password],
+          )
+        if @user.save
+          session[:user_id] = @user.id
+          render json: {
+                   logged_in: true,
+                   user: CustomerRepresenter.new(@user).as_json,
+                 },
+                 status: :created
+        else
+          render json: { errors: @user.errors.full_messages }, status: 500
         end
-        end
+      end
     end
+  end
 end
