5d3cf7bca89a79677a0e15c253fd0689fdb8a919
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-27 22:30:25 +0200

    Update hotels factory

--- spec/factories/hotels.rb
+++ spec/factories/hotels.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
-    factory :hotel do
-        cover_image_url { "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" } 
-        description { "the best Pierre Hotel" }
-        city { "Pierre city"}
-        score {2}
-        price_per_night {99.9}
+  factory :hotel do
+    cover_image_url do
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
     end
+    description { 'the best Pierre Hotel' }
+    city { 'Pierre city' }
+    score { 2 }
+    price_per_night { 99.99 }
+  end
 end
