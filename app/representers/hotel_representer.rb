1b435e5020178a0c2061e298aac5aa45b4c0d91a
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-27 22:31:42 +0200

    Update hotel representer

--- app/representers/hotel_representer.rb
+++ app/representers/hotel_representer.rb
@@ -1,30 +1,29 @@
 class HotelRepresenter
-    
-    def initialize(hotel)
-      @hotel = hotel
-    end
-    def as_json
-       {
+  def initialize(hotel)
+    @hotel = hotel
+  end
+  def as_json
+    {
+      id: hotel.id,
+      name: hotel.hotel_name,
+      slug: hotel.slug,
+      score: hotel.score,
+      price_per_night: hotel.price_per_night,
+      cover_image_url: hotel.cover_image_url,
+      description: hotel.description,
+      city: hotel.city,
+      # name_plus_id: complex_fn(hotel),
+      admin_id: hotel.admin_id,
+      admin_name: hotel.admin.name,
+      created_at: hotel.created_at,
+    }
+  end
 
-                id: hotel.id,
-               name: hotel.hotel_name,
-                slug: hotel.slug,
-                score: hotel.score,
-                price_per_night: hotel.price_per_night,
-                cover_image_url: hotel.cover_image_url,
-                description: hotel.description,
-                city: hotel.city,
+  private
 
-                # name_plus_id: complex_fn(hotel),
-                admin_id: hotel.admin_id,
-                admin_name: hotel.admin.name,
-       }
-    end
-     
-    private
-    attr_reader :hotel
+  attr_reader :hotel
 
-    def complex_fn(hotel)
-        "#{hotel.hotel_name} #{hotel.id}"
-    end
+  def complex_fn(hotel)
+    "#{hotel.hotel_name} #{hotel.id}"
+  end
 end
