f8b673d590db149f1943d51b4b92ae80257e299c
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-28 01:33:00 +0200

    Update routes config

--- config/routes.rb
+++ config/routes.rb
@@ -7,23 +7,25 @@ Rails.application.routes.draw do
       resources :sessions, only: [:create]
       resources :registrations, only: [:create]
 
-      post '/sessions/admin_login', to: "sessions#admin_login"
-      post "sessions/user_login", to: "sessions#user_login"
+      post '/sessions/admin_login', to: 'sessions#admin_login'
+      post 'sessions/user_login', to: 'sessions#user_login'
 
-      delete '/sessions/admin_logout', to: "sessions#admin_logout"
-      delete '/sessions/user_logout', to: "sessions#user_logout"
+      delete '/sessions/admin_logout', to: 'sessions#admin_logout'
+      delete '/sessions/user_logout', to: 'sessions#user_logout'
 
-      get "/sessions/admin_logged_in", to: "sessions#admin_logged_in"
-      get "/sessions/user_logged_in", to: "sessions#user_logged_in"
+      get '/sessions/admin_logged_in', to: 'sessions#admin_logged_in'
+      get '/sessions/user_logged_in', to: 'sessions#user_logged_in'
 
       resources :hotels, param: :slug
-      # resources :reservations, only: [:create], param: :slug
-      get "/reservations", to: "reservations#get_all_reservations"
-      get "/reservations/my_reservations", to: "reservations#get_all_my_reservations"
-      post "/reservations/:slug", to: "reservations#create"
+      resources :customers, param: :email
 
-      resources :admins, only: [:index, :show]
-      resources :customers, only: [:index, :show]
+      get '/reservations', to: 'reservations#get_all_reservations'
+      get '/reservations/my_reservations',
+          to: 'reservations#get_all_my_reservations'
+      post '/reservations/:slug', to: 'reservations#create'
+
+      resources :admins, only: %i[index show]
+      resources :customers, only: %i[index show]
     end
   end
 
