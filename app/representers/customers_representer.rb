92f5f1d19f93c6cdd235a2f807cb94df4dc4e5bb
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-27 22:32:27 +0200

    Create customers representer

---
+++ app/representers/customers_representer.rb
@@ -0,0 +1,21 @@
+class CustomersRepresenter
+  def initialize(customers)
+    @customers = customers
+  end
+  def as_json
+    customers.map do |customer|
+      {
+        id: customer.id,
+        name: customer.name,
+        email: customer.email,
+        created_at: customer.created_at,
+        reservations:
+          ReservationsRepresenter.new(customer.reservations).as_json,
+      }
+    end
+  end
+
+  private
+
+  attr_reader :customers
+end
