73345d679d26aaddc9b41573171797be2da505be
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-28 21:47:48 +0200

    Remove stripe initializer

--- config/initializers/stripe.rb
+++
@@ -1,5 +0,0 @@
-# Rails.configuration.stripe = {
-#     :publishable_key => ENV['PUBLISHABLE_KEY'],
-#     :secret_key => ENV['STRIPE_SECRET_KEY']
-# }
-# stripe.api_key = Rails.configuration.stripe[:secret_key]
