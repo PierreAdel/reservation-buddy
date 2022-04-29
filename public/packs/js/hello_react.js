cebdf921cb114f590cfa83e3c4f466329ff1c1dd
Pierre Adel <pierreadelkamel@gmail.com>
2022-04-29 21:34:20 +0200

    Update .gitignore

---
+++ public/packs/js/hello_react.js
@@ -0,0 +1,18 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["hello_react"],{
+
+/***/ "./app/javascript/hello_react.jsx":
+/*!****************************************!*\
+  !*** ./app/javascript/hello_react.jsx ***!
+  \****************************************/
+/***/ (function() {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: F:\\shakacode\\other\\reservation-buddy-v2\\app\\javascript\\hello_react.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (9:26):\n\n   7 | import PropTypes from \"prop-types\";\n   8 |\n>  9 | const Hello = (props) => <div>Helasdlos {props.name}!</div>;\n     |                          ^\n  10 |\n  11 | Hello.defaultProps = {\n  12 |   name: \"David\",\n\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\n    at instantiate (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:72:32)\n    at constructor (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:358:12)\n    at Parser.raise (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:3335:19)\n    at Parser.expectOnePlugin (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:3392:18)\n    at Parser.parseExprAtom (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:12970:18)\n    at Parser.parseExprSubscripts (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:12540:23)\n    at Parser.parseUpdate (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:12519:21)\n    at Parser.parseMaybeUnary (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:12490:23)\n    at Parser.parseMaybeUnaryOrPrivate (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:12284:61)\n    at Parser.parseExprOps (F:\\shakacode\\other\\reservation-buddy-v2\\node_modules\\@babel\\parser\\lib\\index.js:12291:23)");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ var __webpack_exports__ = (__webpack_exec__("./app/javascript/hello_react.jsx"));
+/******/ }
+]);
